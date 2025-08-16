98342980d47911fc64037bfe0252b2cdc2b9004a
Jungle_log <90323544+logbyjungle@users.noreply.github.com>
2025-08-16 12:35:36 +0200

    nvim

--- keymaps.lua
+++ keymaps.lua
@@ -17,30 +17,30 @@ vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
 -- Buffers
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
-vim.keymap.set('n', '<leader>x', ':Bdelete!<CR>', opts)   -- close buffer
-vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
+-- vim.keymap.set('n', '<leader>x', ':Bdelete!<CR>', opts)   -- close buffer
+-- vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
 
 -- Window management
-vim.keymap.set('n', '<leader>v', '<C-w>v', opts)      -- split window vertically
+-- vim.keymap.set('n', '<leader>v', '<C-w>v', opts)      -- split window vertically
 vim.keymap.set('n', '<leader>h', '<C-w>s', opts)      -- split window horizontally     
 vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
 
 -- Navigate between splits
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+-- vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+-- vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+-- vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+-- vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
 -- Navigate between splits using Ctrl + Arrow Keys
-vim.keymap.set('n', '<C-Up>',    '<C-w>k', opts)
-vim.keymap.set('n', '<C-Down>',  '<C-w>j', opts)
-vim.keymap.set('n', '<C-Left>',  '<C-w>h', opts)
-vim.keymap.set('n', '<C-Right>', '<C-w>l', opts)
+-- vim.keymap.set('n', '<C-Up>',    '<C-w>k', opts)
+-- vim.keymap.set('n', '<C-Down>',  '<C-w>j', opts)
+-- vim.keymap.set('n', '<C-Left>',  '<C-w>h', opts)
+-- vim.keymap.set('n', '<C-Right>', '<C-w>l', opts)
 
 -- Tabs
-vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts)   -- open new tab
-vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
-vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts)     --  go to next tab
-vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts)     --  go to previous tab
+-- vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts)   -- open new tab
+-- vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+-- vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts)     --  go to next tab
+-- vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts)     --  go to previous tab
 
 vim.keymap.set('n', '<F1>', '<Nop>', { noremap = true })
 vim.keymap.set('i', '<F1>', '<Nop>', { noremap = true })
