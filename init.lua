ea16fd12086b494a10fa897eb4b74c67e272a9cb
Jungle_log <90323544+logbyjungle@users.noreply.github.com>
2025-09-20 15:12:39 +0200

    nvim

--- init.lua
+++ init.lua
@@ -277,6 +277,16 @@ require('lazy').setup({
         require("bufferline").setup({})
     end
 },
+{
+    "okuuva/auto-save.nvim",
+    version = '*',
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+    -- your config goes here
+    -- or just leave it empty :)
+    },
+},
 })
 vim.cmd.colorscheme("catppuccin")
 
